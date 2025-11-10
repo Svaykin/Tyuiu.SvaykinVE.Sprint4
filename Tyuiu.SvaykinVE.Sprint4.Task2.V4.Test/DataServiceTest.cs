768fb73d58fc28fb3a95ebc8f5f30bf4506a1a22
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-11-11 02:19:58 +0500

    Task3

--- Tyuiu.SvaykinVE.Sprint4.Task2.V4.Test/DataServiceTest.cs
+++ Tyuiu.SvaykinVE.Sprint4.Task2.V4.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.SvaykinVE.Sprint4.Task2.V4.Test
             DataService ds = new DataService();
             int[] array = { 4, 0, 3, 2, 7, 8, 9, 2, 2, 2, 2, 2, 2 };
             int res = ds.Calculate(array);
-            int wait = 0;
+            int wait = 19;
             Assert.AreEqual(wait, res);
         }
     }
---
+++ Tyuiu.SvaykinVE.Sprint4.Task3.V7/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task3.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #4 | Выполнил: Свайкин В. Е. | АСОиУб-25-1";
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("* Спринт #4                                                                      *");
+        Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                                    *");
+        Console.WriteLine("* Задание #3                                                                     *");
+        Console.WriteLine("* Вариaнт #7                                                                     *");
+        Console.WriteLine("* Выполнил: Свайкин Владислав Евгеньевич | АСОиУб-25-1                           *");
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                       *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статическими  *");
+        Console.WriteLine("* значениями в диапазоне от 1 до 9.Подсчитайте количество нечетных элементов во  *");
+        Console.WriteLine("* всем массиве.                                                                  *");
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+        Console.WriteLine("**********************************************************************************");
+        int[,] mas2 = new int[5, 5] { { 9, 6, 9, 3, 7 },
+                                          { 3, 3, 3, 8, 2 },
+                                          { 2, 1, 3, 5, 2 },
+                                          { 6, 2, 3, 2, 5 },
+                                          { 4, 5, 6, 9, 5 }
+                                                             };
+
+        int rows = mas2.GetUpperBound(0) + 1;
+        int colums = mas2.Length / rows;
+
+        DataService ds = new DataService();
+
+        Console.WriteLine("Массив: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < colums; j++)
+            {
+                Console.WriteLine($"{mas2[i, j]} \t");
+            }
+
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        Console.WriteLine("********************************************************************");
+        Console.WriteLine("РЕЗУЛЬТАТ");
+        Console.WriteLine("********************************************************************");
+
+        int res = ds.Calculate(mas2);
+        Console.WriteLine("Количество нечетных элементов: " + res);
+        Console.ReadKey();
+    }
+}
