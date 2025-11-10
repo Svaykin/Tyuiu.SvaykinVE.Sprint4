7c8c2bcd9bf7f0044e9f1d2bc40acd4e34e7fca5
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-11-11 02:48:22 +0500

    Task4

---
+++ Tyuiu.SvaykinVE.Sprint4.Task4.V30/Program.cs
@@ -0,0 +1,71 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task4.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнил: Свайкин В. Е. | АСОиУб-25-1 ";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Двумерные массивы. (ввод с клавиатуры)                                  *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #30                                                             *");
+        Console.WriteLine("* Выполнил: Свайкин Владислав Евгеньевич | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 7. Заменить нечетные        *");
+        Console.WriteLine("* элементы массива на 0.                                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите количество строк массива:");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов массива: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, columns];
+
+        Console.WriteLine("***************************************************************************");
+
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите элемент массива {i},{j}:");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Введённый массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        int[,] res = ds.Calculate(matrix);
+
+        Console.WriteLine("Массив без нечётных элементов:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{res[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
