44fc3703cdfa8bd824dda868f3daad6b2b63b8e7
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-11-11 01:35:16 +0500

    Task2

---
+++ Tyuiu.SvaykinVE.Sprint4.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task2.V4.Lib;
+namespace Tyuiu.SvaykinVE.Sprint4.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 0, 3, 2, 7, 8, 9, 2, 2, 2, 2, 2, 2 };
+            int res = ds.Calculate(array);
+            int wait = 0;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SvaykinVE.Sprint4.Task2.V4/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task2.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+        Console.Title = "Спринт #4 | Выполнил: Свайкин В. Е. | АСОиУб-25-1";
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("* Спринт #4                                                                      *");
+        Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                          *");
+        Console.WriteLine("* Задание #2                                                                     *");
+        Console.WriteLine("* Вариaнт #4                                                                     *");
+        Console.WriteLine("* Выполнил: Свайкин Владислав Евгеньевич | АСОиУб-25-1                           *");
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                       *");
+        Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный случайными     *");
+        Console.WriteLine("* в диапазоне от 2 до 9 подсчитать сумму четных элементов массива.               *");
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                               *");
+        Console.WriteLine("**********************************************************************************");
+        int len = Convert.ToInt32(Console.ReadLine());
+        int[] numsArray = new int[len];
+        for (int i = 0; i <= len - 1; i++)
+        {
+            numsArray[i] = rnd.Next(2, 9);
+        }
+        Console.WriteLine("Массив:");
+        for (int i = 0; i <= len - 1; i++)
+        {
+            Console.WriteLine(numsArray[i] + " ");
+        }
+        Console.WriteLine("**********************************************************************************");
+        Console.WriteLine("*РЕЗУЛЬТАТ:                                                                      *");
+        Console.WriteLine("**********************************************************************************");
+        int p = Convert.ToInt32(ds.Calculate(numsArray));
+        Console.WriteLine(p);
+        Console.ReadKey();
+    }
+}
