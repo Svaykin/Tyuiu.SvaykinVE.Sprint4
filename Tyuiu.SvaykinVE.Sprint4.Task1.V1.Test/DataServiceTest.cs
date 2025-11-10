d264862bc9f205f59795dcd0b2c6c38210ded16b
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-11-10 07:16:13 +0500

    Task1

---
+++ Tyuiu.SvaykinVE.Sprint4.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task1.V1.Lib;
+namespace Tyuiu.SvaykinVE.Sprint4.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 8, 2, 7, 5, 0, 7, 4, 7, 5, 7 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 14;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
