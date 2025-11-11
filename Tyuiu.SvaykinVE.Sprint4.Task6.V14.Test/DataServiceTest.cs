db19d53c4ff40c89f8a53de439678fed3ee9ddc2
Tyuiu.SvaykinVE@outlook.com <svaykin07@mail.ru>
2025-11-12 03:29:36 +0500

    Task6

---
+++ Tyuiu.SvaykinVE.Sprint4.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task6.V14.Lib;
+namespace Tyuiu.SvaykinVE.Sprint4.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            var months = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль" };
+
+            string[] res = ds.Calculate(months);
+
+            Assert.AreEqual(6, res.Length);
+            CollectionAssert.AreEqual(new string[] { "Январь", "Февраль", "Март", "Апрель", "Июнь", "Июль" }, res);
+        }
+    }
+}
---
+++ Tyuiu.SvaykinVE.Sprint4.Task6.V14/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.SvaykinVE.Sprint4.Task6.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт 4 | Выполнила: Свайкин В. Е. | АСОиУб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Класс Array                                                       *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнил: Свайкин Владислав Евгеньевич | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан строковый массив данных [\"Январь\", \"Февраль\", \"Март\",         *");
+        Console.WriteLine("* \"Апрель\", \"Май\", \"Июнь\", \"Июль\"], используя класс Array,        *");
+        Console.WriteLine("* выведите элементы массива, длина которых больше 3 символов.             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        var months = new string[] { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль" };
+        Console.WriteLine("Исходный массив: ");
+        Console.WriteLine(String.Join(", ", months));
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string[] res = ds.Calculate(months);
+        Console.WriteLine("Элементы массива, длина которых больше 3 символов: ");
+        foreach (var month in res)
+        {
+            Console.WriteLine(month);
+        }
+
+        Console.ReadKey();
+    }
+}
